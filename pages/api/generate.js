e5fb2141ee39aac33ed8dc3ce6578b334fc2c592
AriaWorkshop <ariaworkshopnft@gmail.com>
2023-01-13 00:57:27 +0200

    commit

---
+++ pages/api/generate.js
@@ -0,0 +1,27 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = "I am at a therapy session with a psychotherapy specialist and I am the pacient \n Me: ";
+const basePrompSuffix = "\n, what's your advice?  The therapist : "
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix + " "}${req.body.userInput}${" " + basePrompSuffix}`,
+    temperature: 0.8,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
